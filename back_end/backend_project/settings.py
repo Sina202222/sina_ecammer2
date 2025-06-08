1777ba1056686969550b92382d5da717b7358e1e
Sina202222 <sina.hashemolhossainy@gmail.com>
2025-06-08 05:17:22 +0330

    install package React

--- back_end/backend_project/settings.py
+++ back_end/backend_project/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# برای ارتباط با React (اجازه درخواست از فرانت‌اند)
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
